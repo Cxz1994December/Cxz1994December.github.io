70a244b56b2e7e69bb17136ab3221561a6128cb7
cxz1994December <574998065@qq.com>
2018-05-07 10:21:47 +0800

    qianzhu

--- js/index.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $('.nav li').click(function() {
-        $(this).addClass('active_color').siblings().removeClass('active_color');
-        $('.cent li').eq($(this).index()).addClass('active').siblings().removeClass('active');
-    });
-});
